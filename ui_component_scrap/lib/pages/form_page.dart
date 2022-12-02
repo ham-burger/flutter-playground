6d65c49de36702db5f9a4d1018e638c4c50fc689
ham-burger <devkudou@gmail.com>
2022-12-02 09:50:48 +0900

    feat(ui): add date-picekr to form-page

--- ui_component_scrap/lib/pages/form_page.dart
+++ ui_component_scrap/lib/pages/form_page.dart
@@ -11,6 +11,8 @@ class _FormPageState extends State<FormPage> {
   TextEditingController textEditingController =
       TextEditingController(text: 'text');
 
+  String _calenderText = '0001/01/01';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +40,33 @@ class _FormPageState extends State<FormPage> {
               labelText: 'TextField',
               hintText: 'hint',
             ),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100),
+                helpText: 'helpText',
+                cancelText: 'cancelText キャンセル',
+                confirmText: 'confirmText OK',
+                selectableDayPredicate: (DateTime day) {
+                  return day.weekday != 6 && day.weekday != 7;
+                  // return day.day != DateTime.now().day;
+                },
+                // currentDate: DateTime.now(),
+              ).then(
+                (value) {
+                  setState(
+                    () {
+                      _calenderText = value!.toString();
+                    },
+                  );
+                },
+              );
+            },
+            child: Text(_calenderText),
           ),
         ],
       ),
